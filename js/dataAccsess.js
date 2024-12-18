3beccf0c0feb0d5065a08ef74f37ed5ae1458c97
Andrei Beloziyorove <andreinw12@gmail.com>
2024-12-18 17:05:22 +0200

    add dataaccss file with all the documentation

---
+++ js/dataAccsess.js
@@ -0,0 +1,17 @@
+//this js code will handle databace on local storge 
+//the build of the data will be in this tracture:
+// local storage:{
+// registration:
+// {
+// key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
+// key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
+// key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
+// key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
+// }}
+// {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]} is json structure that we will named registration
+
+
+function saveRegistration(presonRegistration){} //get json of person and enter it into the local stoege
+function getAllRegisration(){} //get from the local storge all the registration
+function getAllRegistrationByDate(date){} //get all the registration in date 
+function idMaker(){} // give a unick key to 
