dbd25522194955ec3ecb421128cf879666c667c1
Andrei Beloziyorove <andreinw12@gmail.com>
2024-12-18 22:41:54 +0200

    Co-authored-by: רפאל ביטון <Refael.Bitton@e.braude.ac.il> Co-authored-by: asafe51@windowslive.com <asafe51@windowslive.com> Co-authored-by: הוד ווקרט <Hod.Vakrat@e.braude.ac.il>
    add locals storge and update all the site to using this local storge.

--- js/dataAccsess.js
+++ js/dataAccsess.js
@@ -1,17 +1,63 @@
-//this js code will handle databace on local storge 
-//the build of the data will be in this tracture:
+//this js code will handle database on local storage 
+//the build of the data will be in this structure:
 // local storage:{
-// registration:
+// registrations:
 // {
 // key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
 // key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
 // key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
 // key: {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]},
-// }}
+// }
+// services§:
+//{}
+//
+//}
 // {name: "", Email: "", phone:"",date:"",time"",service:"",extra:[1,2,3]} is json structure that we will named registration
 
+//get json of person and enter it into the local storage
+function saveRegistration(personRegistration){
+    let registrations = getAllRegistration();
+    let uniqueKey = idMaker();
+    registrations[uniqueKey] = personRegistration;
+    localStorage.setItem('registrations', JSON.stringify(registrations));
+    return uniqueKey;
+} 
+// give a unique key to 
+function idMaker(){
+    return 'reg_' + Date.now() + '_' + Math.random().toString(36);
+} 
+//get from the local storage all the Registration
+function getAllRegistration(){
+    return JSON.parse(localStorage.getItem('registrations')) || {};
+} 
+//get all the Registration in date 
+function getAllRegistrationByDate(date){
+    let registrations = getAllRegistration();
+    let filteredRegistrations = {};
+    for( let key in registrations) {
+        let registration  = registrations[key];
+        if(registration.date === date){
+            filteredRegistrations[key] = registration;
+        }
+    }
+    return filteredRegistrations;
+} 
 
-function saveRegistration(presonRegistration){} //get json of person and enter it into the local stoege
-function getAllRegisration(){} //get from the local storge all the registration
-function getAllRegistrationByDate(date){} //get all the registration in date 
-function idMaker(){} // give a unick key to 
+function getAllUsedTimesByDate(date){
+    let allRegInDate = getAllRegistrationByDate(date);
+    let usedTime = []
+    for(let key in allRegInDate)
+    {
+        usedTime.push(allRegInDate[key].time);
+    }
+    return usedTime;
+}
+
+// function getServices(){
+//     let registrations = getAllRegistration();
+//     let services = {"haircut":0,"beard_trim":0,"full_package":0};
+//     for (let key in registrations) {
+        
+//     }
+// }
+
--- js/registration.js
+++ js/registration.js
@@ -1,7 +1,7 @@
 let dateChange = false;
-let tiemChange = false;
+let timeChange = false;
 let date = document.getElementById("idDate");
-let time = document.getElementById("idTime");
+
 const serviceRadios = document.querySelectorAll('input[name="services"]');
     
 // Select all extra service checkboxes
@@ -10,14 +10,17 @@ const extraCheckboxes = document.querySelectorAll('input[name="extraServices"]')
 // Select the total price display element
 const totalPriceDiv = document.getElementById('totalPrice');
 
+let timeCombobox = document.getElementById('idTime');
+
 
 function register(){
     let fullname = document.getElementById("idName");
     let email = document.getElementById("idEmail");
     let phone = document.getElementById("idphoneNumber");
 
-    let radioServis = document.getElementsByName("services");
-    //TODO: save the extra service
+    let radioServices = radioChecked(document.getElementsByName("services"));
+    let extras = getExtras(extraCheckboxes);
+
     if (fullname.value == "") { // checks if user enter name
         alert("enter name");
         return;
@@ -34,24 +37,41 @@ function register(){
         alert("need to enter date");
         return;
     }
-    if (tiemChange == false){// no enter time 
-        alert("need to enter time");
+    if (timeCombobox.value == "")
+    {
+        alert("change date");
         return;
     }
-    if (!radioCheckd(radioServis)){ // no enter servise
+    if (radioServices == "no time"){ // no enter servise
         alert("need to enter service");
         return;
     }
-    //TODO: change the this to make dicunary and save it with dataAccess.js
-    alert(`thank you Mr,/Mrs, ${fullname.value} for Book hair cut on date: ${date.value} in time: ${time.value}. we will call you to this number ${phone.value}`);
+    let personRegistration = {
+        name: fullname.value,
+        email: email.value,
+        phone: phone.value,
+        date: date.value,
+        time: time.value,
+        service: radioServices,
+        extras: extras
+    };
+    //{name: "", Email: "", phone:"",date:"",time"",service:"",extra:["",""]} 
+    saveRegistration(personRegistration);
+    clearTime();
+    //alert(`thank you Mr,/Mrs, ${fullname.value} for Book hair cut on date: ${date.value} in time: ${time.value}. we will call you to this number ${phone.value}`);
     document.getElementById("appointment-form").reset();
     
 }
 
 
-// if user enter his date and time (not defult date/time)
-date.addEventListener("change", function(){ dateChange = true;}) // TODO: change this function aftet every time that we change the date we will add or remove time in drop box
-time.addEventListener("change", function(){ tiemChange = true;})
+// if user enter his date and time (not default date/time)
+date.addEventListener("change", function(){ 
+    clearTime();
+    dateChange = true;
+    updateTimesByDate(date.value);
+});
+
+// TODO: change this function after every time that we change the date we will add or remove time in drop box
 
 function calculateTotal() {
     let total = 0;
@@ -82,24 +102,58 @@ extraCheckboxes.forEach(checkbox => {
     checkbox.addEventListener('change', calculateTotal);
 });
 
-
-// check if the user select servise
-function radioCheckd(radioServis){
-    for (let i = 0; i < radioServis.length; i++) {
-        if (radioServis[i].checked) 
-            return true; //select
+//getting customer's selected service:
+function radioChecked(radioServices){
+    for (let i = 0; i < radioServices.length; i++) {
+        if (radioServices[i].checked) 
+            return radioServices[i].value; //select
     }
-    return false; // not celect 
+    return ''; // not select 
 }
 
+//getting all of the customer's selected extra services
+function getExtras(checkboxes){
+    selected = []
+    for (let i = 0; i < checkboxes.length; i++){
+        if (checkboxes[i].checked)
+            selected.push(checkboxes[i].value);
+    }
+    return selected;
+}
 
-// check valid phone with regular exprestion 
+function clearTime(){
+    while (timeCombobox.length > 0) timeCombobox.remove(0);
+}
+
+function updateTimesByDate(date){
+    let used = getAllUsedTimesByDate(date);
+    for (let i = 8; i < 17; i++){
+        let timeUsed = false;
+        for (let j = 0; j < used.length; j++){
+            if (Number(used[j].substring(0, 2)) == i){
+                timeUsed = true;
+                break;
+            }
+        }
+        if (!timeUsed){
+            let option = document.createElement("option");
+            option.text = (i < 10) ? `0${i}:00` : `${i}:00`;
+            timeCombobox.add(option);
+        }
+    }
+    if (timeCombobox.options.length == 0){
+        document.getElementById('timelbl').value = "No Time Available";
+        timeCombobox.add("no time");
+    } 
+}
+
+// check valid phone with regular expression 
 function isValidPhoneNumber(number){
     const pattern = /^05\d{8}$/;
     return pattern.test(number);
 }
 
-// check valid Email with regular exprestion 
+// check valid Email with regular expression 
 function isValidEmail(email){
     const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     return pattern.test(email);
